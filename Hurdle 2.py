9f850fc6db70eca7195087c38d0e97c7ac6d098c
adityaasaadhakim <149054857+adityaasaadhakim@users.noreply.github.com>
2023-11-15 10:47:49 +0700

    Add files via upload

---
+++ Hurdle 2.py
@@ -0,0 +1,21 @@
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+def Jump():
+    move()
+    turn_left()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    turn_left()
+
+Jump()
+Jump()
+
+################################################################
+# WARNING: Do not change this comment.
+# Library Code is below.
+################################################################
---
+++ Hurdle 3.py
@@ -0,0 +1,30 @@
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+def Jump():
+    turn_left()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    turn_left()
+    
+move()
+move()
+move()
+move()
+Jump()
+Jump()
+Jump()
+move()
+move()
+move()
+Jump()
+move()
+
+################################################################
+# WARNING: Do not change this comment.
+# Library Code is below.
+################################################################
